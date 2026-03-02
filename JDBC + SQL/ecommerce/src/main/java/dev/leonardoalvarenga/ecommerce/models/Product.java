6f550e5e838733844e5fc811d57e16cae0fec667
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-02 08:25:22 -0300

    feat: add Product entity

---
+++ JDBC + SQL/ecommerce/src/main/java/dev/leonardoalvarenga/ecommerce/models/Product.java
@@ -0,0 +1,54 @@
+package dev.leonardoalvarenga.ecommerce.models;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+    private int stock;
+
+    public Product(){}
+
+    public Product(String name, double price, int stock){
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
+}
