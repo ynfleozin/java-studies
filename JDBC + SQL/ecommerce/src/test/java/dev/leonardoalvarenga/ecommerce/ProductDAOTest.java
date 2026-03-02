50f06a9535c64217a4555b6d2ae2098508ae7d18
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-02 08:51:05 -0300

    test: check that the product has been saved successfully

--- JDBC + SQL/ecommerce/src/test/java/dev/leonardoalvarenga/ecommerce/ProductDAOTest.java
+++ JDBC + SQL/ecommerce/src/test/java/dev/leonardoalvarenga/ecommerce/ProductDAOTest.java
@@ -1,6 +1,7 @@
 package dev.leonardoalvarenga.ecommerce;
 
 import dev.leonardoalvarenga.ecommerce.DAO.ProductDAO;
+import dev.leonardoalvarenga.ecommerce.models.Product;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,5 +12,15 @@ public class ProductDAOTest {
         ProductDAO productDAO = new ProductDAO();
 
         assertDoesNotThrow(productDAO::createTableIfNotExists);
+    }
+
+    @Test
+    public void shouldSaveProductSuccessfully(){
+        Product product = new Product("Notebook", 4000, 10);
+        ProductDAO productDAO = new ProductDAO();
+
+        assertDoesNotThrow(() -> {
+            productDAO.save(product);
+        });
     }
 }
