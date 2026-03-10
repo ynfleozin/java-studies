f7a5c1058cb9f9569c367890178e72e8e9e7bf00
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-10 17:02:39 -0300

    feat: add report service to process match data

---
+++ steams + optional/championship-reports/src/main/java/dev/leonardoalvarenga/championshipreports/services/ReportService.java
@@ -0,0 +1,13 @@
+package dev.leonardoalvarenga.championshipreports.services;
+
+import dev.leonardoalvarenga.championshipreports.models.Match;
+
+import java.util.List;
+
+public class ReportService {
+    public long countMatchesByTeam(List<Match> matches, String teamName){
+        return matches.stream()
+                .filter(match -> match.homeTeam().equals(teamName) || match.awayTeam().equals(teamName))
+                .count();
+    }
+}
