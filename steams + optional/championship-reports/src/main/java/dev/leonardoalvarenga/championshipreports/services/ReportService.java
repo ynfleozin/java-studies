ab8baec6e3e720d896756ae61f22627498807702
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-11 15:37:52 -0300

    feat: create function to find first match by team using Optional

--- steams + optional/championship-reports/src/main/java/dev/leonardoalvarenga/championshipreports/services/ReportService.java
+++ steams + optional/championship-reports/src/main/java/dev/leonardoalvarenga/championshipreports/services/ReportService.java
@@ -3,6 +3,7 @@ package dev.leonardoalvarenga.championshipreports.services;
 import dev.leonardoalvarenga.championshipreports.models.Match;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ReportService {
     public long countMatchesByTeam(List<Match> matches, String teamName){
@@ -15,5 +16,11 @@ public class ReportService {
         return matches.stream()
                 .mapToInt(Match::getTotalGoals)
                 .sum();
+    }
+
+    public Optional<Match> findFirstMatchByTeam(List<Match> matches, String teamName){
+        return matches.stream()
+                .filter(match -> match.homeTeam().equals(teamName) || match.awayTeam().equals(teamName))
+                .findFirst();
     }
 }
